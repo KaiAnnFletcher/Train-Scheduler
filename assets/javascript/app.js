1c67e9fd158363420fd9e8d73022c073b7add1a6
KaiAnnFletcher <kaiann.fletcher@mail.utoronto.ca>
2019-03-20 20:09:07 -0400

    Bug Fixes (v1)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCaANtWiObSDj08dW55YoyaAPf696NgHbk",
+    authDomain: "train-scheduler-a5eba.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a5eba.firebaseio.com",
+    projectId: "train-scheduler-a5eba",
+    storageBucket: "train-scheduler-a5eba.appspot.com",
+    messagingSenderId: "156527369811"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
 /*Declare all variables that need to be 
 added to div sections dynamically*/ 
 var trainFrequency = 5;
@@ -29,9 +43,48 @@ console.log("MINUTES TILL TRAIN: " + minutesTillTrain);
 var nextTrain = moment().add(minutesTillTrain, "minutes");
 console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-var trainName = ""
-var destination = ""
-$('#submit').on('click', function(event) {
+var trainName = $("#form-train-name").val().trim();
+var destination = $("#form-destination").val().trim();
+
+console.log(trainName);
+console.log(destination);
+debugger;
+//Save Firebase data
+database.ref("/trainData").on("value", function(snapshot) {
+
+    if (snapshot.child("trainName").exists() && snapshot.child("destination").exists() && snapshot.child("trainFrequency").exists() && snapshot.child("firstTimeConverted").exists()) {
+
+      //set the variables to be equal to the variables that above related to to the moment calculations
+      trainName = snapshot.val().trainName;
+      destination = snapshot.val().destination;
+      trainFrequency = snapshot.val().trainFrequency;
+      firstTimeConverted = snapshot.val().firstTimeConverted;
+
+      console.log(snapshot.val().trainName);
+      console.log(snapshot.val().destination);
+      console.log(snapshot.val().trainFrequency);
+      console.log(snapshot.val().firstTimeConverted);
+    }
+
+    $("#submit").on("click", function(event) {
+      event.preventDefault();
+    
+      //change the HTML on submit
+    
+      $("#train-name").text(snapshot.val().trainName);
+      $("#destination").text(snapshot.val().destination);
+      $("#frequency").text(snapshot.val().trainFrequency);
+      $("#next-arrival").text(nextTrain);
+      $("#minutes-away").text(minutesTillTrain);
+    });
+});
+
+function errorHandling (errorObject) {
+console.log("The read failed: " + errorObject.code);
+}
+
+
+/*$('#submit').on('click', function(event) {
 
 event.preventDefault();
 
@@ -46,4 +99,6 @@ console.log(destination);
 firstTime= $('#form-first-train-time').val().trim();
 $("#next-arrival").append(nextTrain);
 console.log(nextTrain);
-});
+});*/
+
+//create a new div with each time the history is recalled from firebase
